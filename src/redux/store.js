f10f3b0798ba3db6bccc459e3d9702cc68ebfa2f
ugurlums <ugurlumsaid@gmx.de>
2024-01-24 17:48:15 +0100

    basically data and modal slice for redux in

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import dataSlice from './dataSlice'
+import { modalSlice } from './modalSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+data: dataSlice,
+modal: modalSlice
+
+  },
 })
