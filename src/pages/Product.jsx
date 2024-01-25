40b3031eac37f49372b91d07f4eda484be176c93
ugurlums <ugurlumsaid@gmx.de>
2024-01-25 13:05:55 +0100

    model component created and connected

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,13 +1,15 @@
 import { useSelector } from "react-redux";
 import ProductCard from "../components/ProductCard";
+import Modal from "../components/Modal";
 
 const Product = () => {
   const { modal } = useSelector((state) => state.modal);
-  
+
   console.log(modal, "modal");
   return (
     <div className="">
       <ProductCard />
+      {modal && <Modal/> }
     </div>
   );
 };
